9efd4b589e8449849b29bf70901c532e7fe0fedd
Liam Liden <l14m@msn.com>
2020-08-26 14:31:00 -0500

    Test transitions with new backgrounds

--- js/backgroundHover.js
+++ js/backgroundHover.js
@@ -1,11 +1,11 @@
 $(document).ready(function(){
     $("#leadership").mouseover(function(){
-        $("#body").css("background-image", "url('../assets/brianCircle.png')");
+        $("#body").css("background-image", "url('../assets/background1.png')");
     });
 });
 
 $(document).ready(function(){
     $("#service").mouseover(function(){
-        $("#body").css("background-image", "url('../assets/brian.jpg')");
+        $("#body").css("background-image", "url('../assets/background2.jpg')");
     });
 });
