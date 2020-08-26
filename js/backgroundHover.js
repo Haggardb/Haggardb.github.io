5ac627075c0904c3b7429075b5efd69aa0a3ad81
Liam Liden <l14m@msn.com>
2020-08-26 13:47:49 -0500

    Fix responsiveness of centered headers

---
+++ js/backgroundHover.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+    $("#leadership").mouseover(function(){
+        $("#body").css("background", "url('../assets/brianCircle.png') no-repeat center center fixed");
+    });
+});
+
+$(document).ready(function(){
+    $("#service").mouseover(function(){
+        $("#body").css("background", "url('../assets/brian.jpg') no-repeat center center fixed");
+    });
+});
